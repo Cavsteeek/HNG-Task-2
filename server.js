d1542e805612145b49ec606941ff76e791a32ee8
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-10-20 15:58:55 +0100

    commit

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ import 'dotenv/config';
 
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send("API working"));
-app.use('/r', router);
+app.use(cors({ credentials: true }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => res.send("API working"));
+app.use('/', router);
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
